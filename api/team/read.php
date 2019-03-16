80adb8cdbe0ba9341496709c695bc645649542af
James Carr <carr5618@gmail.com>
2019-03-16 13:58:43 -0400

    Reading Teams From Database

---
+++ api/team/read.php
@@ -0,0 +1,54 @@
+<?php
+
+// Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+require_once '../../config/Database.php';
+require_once '../../models/Team.php';
+include_once '../../models/ErrorHandler.php';
+include_once '../../models/SuccessHandler.php';
+
+// Instantiate Database
+$database = new Database();
+$db = $database->connect();
+
+// Instantiate Team, send in database conenction
+$team = new Team($db);
+
+// Instantiate Success Message Handler
+$success = new SuccessHandler();
+
+// Instantiate Error Message Handler
+$error = new ErrorHandler();
+
+// Read all the teams
+$results = $team->read();
+
+// Get the row count of the teams
+$num = $results->rowCount();
+
+// Check if any teams exist
+if($num > 0){
+  // Team Array to print out
+  $team_arr = [];
+  while($row = $results->fetch(PDO::FETCH_ASSOC)){
+    extract($row);
+    $team = [
+      'id' => $team_ID,
+      'name' => $name,
+      'win' => $win,
+      'loss' => $loss,
+      'championship' => $championship
+    ];
+
+    // Push team into team array
+    array_push($team_arr, $team);
+  }
+
+  echo json_encode($team_arr);
+}
+else{
+  // No teams to display
+  echo $success->;
+}
--- config/Database.php
+++ config/Database.php
@@ -9,13 +9,13 @@ class Database{
 
   public function connect(){
     try{
-      $con = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name,$this->username,$this->password);
+      $this->con = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name,$this->username,$this->password);
       $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e){
       echo 'connection error ' . $e;
     }
 
 
-    return $this->con; 
+    return $this->con;
   }
 }
